01fd941682203033d47b53a324452d43d731982e
黄滟雯 <2017013561@secoder.net>
2020-11-28 16:57:15 +0800

    Add sight and dist control.

---
+++ IoThingsLab/Assets/MyDistItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyDistItem : MonoBehaviour
+{
+    private Camera mainCamera;	
+
+    void Start() {
+        Debug.Log("Dist Start!!!");
+		if (!mainCamera)
+			mainCamera = Camera.main;
+    }
+
+    void Update() {
+        if (transform.position[0] - mainCamera.transform.position[0] > 0 && transform.position[0] - mainCamera.transform.position[0] < 5 && System.Math.Abs(transform.position[2] - mainCamera.transform.position[2]) < 2.2)
+            Debug.Log("In dist!");
+        else
+            Debug.Log("Out of dist!");
+    }
+}
---
+++ IoThingsLab/Assets/MyGazeItem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyGazeItem : MonoBehaviour
+{
+    private Camera mainCamera;	
+	
+	void Start() {
+        Debug.Log("Gaze Start!!!");
+		if (!mainCamera)
+			mainCamera = Camera.main;
+	}
+	
+	void Update() {
+        Vector3 vec = mainCamera.WorldToViewportPoint(transform.position);
+        if (vec.x > 0 && vec.x < 1 && vec.y > 0 && vec.y < 1 && vec.z > mainCamera.nearClipPlane && vec.z < mainCamera.farClipPlane)
+            Debug.Log("In sight!");
+        else
+            Debug.Log("Out of sight");
+	}
+}
